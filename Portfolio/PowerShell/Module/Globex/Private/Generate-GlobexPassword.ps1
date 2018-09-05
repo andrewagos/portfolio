8949360a28c1d657dfc35ee1457e520a55485676
andrewagos <43009860+andrewagos@users.noreply.github.com>
2018-09-05 11:51:24 -0500

    Update Generate-GlobexPassword.ps1

--- Portfolio/PowerShell/Module/Globex/Private/Generate-GlobexPassword.ps1
+++ Portfolio/PowerShell/Module/Globex/Private/Generate-GlobexPassword.ps1
@@ -1,6 +1,5 @@
-function Generate-HarvardPassword {
+function Generate-GlobexPassword {
    [CmdletBinding()]
-   #there is a better way to do this I'm sure.
    $GUID = [guid]::NewGuid().guid.split('-')
  
    return ("Globex" + $GUID[1])
